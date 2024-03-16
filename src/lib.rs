12a34a2c0aadb2869c43c0481c4d17cddc7f1e0b
Possseidon <xpossseidon@gmail.com>
2024-03-16 13:08:24 +0100

    Add endianness feature flags
    Also enable `rkyv/strict` feature.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
 pub mod config;
 pub mod net;
 pub mod packet;
+
+#[cfg(not(any(feature = "packet_le", feature = "packet_be", feature = "packet_ne")))]
+compile_error!("enable either packet_le or packet_be; if you are 100% sure you will never send a packet between computers with different endianness, enable packet_ne");
+
+#[cfg(all(feature = "packet_le", feature = "packet_be"))]
+compile_error!("packet_le and packet_be are mutually exclusive");
+
+#[cfg(all(feature = "packet_le", feature = "packet_ne"))]
+compile_error!("packet_le and packet_ne are mutually exclusive");
+
+#[cfg(all(feature = "packet_be", feature = "packet_ne"))]
+compile_error!("packet_be and packet_ne are mutually exclusive");
